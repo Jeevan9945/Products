5394e055cbb2cad53ada88a6ff410e627e9fd510
Jeevan K A <jeevanashok9945@gmail.com>
2026-05-09 23:09:15 +0530

    Update CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ datamodel.Base.metadata.create_all(bind=engine)
 app=FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
